663f3b422c4c59bd22004da05540459ca9e09631
William Shelley <wjshelley5018@gmail.com>
2020-08-05 13:07:44 -0400

    WIP commented out UsersController#index

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,29 +2,29 @@ class Api::UsersController < ApplicationController
     def index
         @users = [current_user]
         
-        if filter_params
-            if filter_params[:page_owner_id]
-                @users << User.includes(:profile_photo_attachment,:cover_photo_attachment)
-                .find_by_id(filter_params[:page_owner_id])
-            end
+        # if filter_params
+        #     if filter_params[:page_owner_id]
+        #         @users << User.includes(:profile_photo_attachment,:cover_photo_attachment)
+        #         .find_by_id(filter_params[:page_owner_id])
+        #     end
 
-            if filter_params[:user_ids]
-                @users += User
-                .includes(:profile_photo_attachment, :cover_photo_attachment)
-                .select("*")
-                .where("id IN (?)", filter_params[:user_ids])
+        #     if filter_params[:user_ids]
+        #         @users += User
+        #         .includes(:profile_photo_attachment, :cover_photo_attachment)
+        #         .select("*")
+        #         .where("id IN (?)", filter_params[:user_ids])
                 
-            elsif filter_params[:name]
-                start_of_name = filter_params[:name] ? filter_params[:name] : ""
-                @users += User
-                .includes(:profile_photo_attachment, :cover_photo_attachment)
-                .select("*")
-                .where("CONCAT(first_name, ' ', last_name) LIKE ?%", start_of_name)
+        #     elsif filter_params[:name]
+        #         start_of_name = filter_params[:name] ? filter_params[:name] : ""
+        #         @users += User
+        #         .includes(:profile_photo_attachment, :cover_photo_attachment)
+        #         .select("*")
+        #         .where("CONCAT(first_name, ' ', last_name) LIKE ?%", start_of_name)
                 
-            elsif filter_params[:all_users]
-                @users = User.includes(:profile_photo_attachment,:cover_photo_attachment).all
-            end
-        end
+        #     elsif filter_params[:all_users]
+        #         @users = User.includes(:profile_photo_attachment,:cover_photo_attachment).all
+        #     end
+        # end
 
         render :index
     end
